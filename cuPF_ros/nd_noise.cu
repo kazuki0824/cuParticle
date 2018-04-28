be3924c59c5ed200d942d0b8c4286e39c45937c5
充輝 <prog.ten_fingers_2@ezweb.ne.jp>
2018-04-29 04:26:49 +0900

    尤度と状態遷移の実装

--- cuPF_ros/nd_noise.cu
+++ cuPF_ros/nd_noise.cu
@@ -21,9 +21,40 @@ __device__ float3 getSystemNoise(curandState_t * s, mean_var_param param)
 	return v;
 }
 
-__device__ float3 suggest_distribution(float3 x,float3 system_noise, float3 speed)
+__device__ float3 suggest_distribution(float3 x, float3 speed, curandState_t *s)
 {
 	//TODO:状態遷移関数?
-	return x + system_noise;
+
+	//int thId=threadIdx.x+ blockDim.x * blockIdx.x; //今のパーティクルの番号
+	float delta_rot1, delta_trans, delta_rot2;
+	float delta_rot1_, delta_trans_, delta_rot2_;
+	float alpha[4] = {1.0, 1.0, 1.0, 1.0};
+	float3 result;
+
+	delta_rot1 = atan2f(speed.y, speed.x) - x.z;
+	delta_trans = sqrtf(speed.y * speed.y + speed.x * speed.x);
+	delta_rot2 = speed.z - delta_rot1;
+
+	delta_rot1_  = (delta_rot1
+					- (  alpha[0] * powf(delta_rot1, 2)
+					   + alpha[1] * powf(delta_trans, 2)
+					  )
+				   ) * curand_normal(s);
+	delta_trans_ = (delta_trans
+					- (  alpha[2] * powf(delta_trans, 2)
+					   + alpha[3] * powf(delta_rot1, 2)
+					   + alpha[3] * powf(delta_rot2, 2)
+					  )
+				   ) * curand_normal(s);
+	delta_rot2_  = (delta_rot2
+					- (  alpha[0] * powf(delta_rot2, 2)
+					   + alpha[1] * powf(delta_trans, 2)
+					  )
+				   ) * curand_normal(s);
+	result.x = x.x + delta_trans_ * cosf(x.z + delta_rot1_);
+	result.y = x.y + delta_trans_ * sinf(x.z + delta_rot1_);
+	result.z = x.z + delta_rot1_ + delta_rot2_;
+
+	return result;
 }
 
