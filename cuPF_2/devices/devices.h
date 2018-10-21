37f38b1a172c2c129164a041d1c10efa3bc84195
zohar <zoharisikenuma@gmail.com>
2018-10-21 10:29:09 +0900

    シミュレーション用環境を構築しました

---
+++ cuPF_2/devices/devices.h
@@ -0,0 +1,17 @@
+#ifndef DEF_DEVICES_H
+
+#define DEF_DEVICES_H
+
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "geometry_msgs/Pose.h"
+
+// コールバック関数
+void scanCallback(const sensor_msgs::LaserScan&);
+void poseCallback(const geometry_msgs::Pose&);
+
+// パラメーター群
+extern float threshold_transform;
+extern float threshold_rotate;
+
+#endif
